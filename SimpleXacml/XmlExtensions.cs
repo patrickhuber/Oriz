e4625ef89a9be5f2db2c04da7935dd74d4edf9a1
Patrick Huber <prhuber@gmail.com>
2014-01-19 14:19:49 -0500

    adds xml extensions

---
+++ SimpleXacml/XmlExtensions.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Xml;
+using System.Xml.Linq;
+
+namespace SimpleXacml
+{
+    public static class XmlExtensions
+    {
+        public static XmlNode ToXmlNode(this string value)
+        {            
+            var document = new XmlDocument();
+            return document.CreateTextNode(value);            
+        }
+    }
+}
