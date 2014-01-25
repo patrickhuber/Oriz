ee3c68f004c093a72fd05521e3d26e6a1c6ddd24
Patrick Huber <prhuber@gmail.com>
2014-01-25 12:03:25 -0500

    adds composition container as default resolver

---
+++ Xacml/CompositionDependencyResolver.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Composition;
+using System.ComponentModel.Composition.Hosting;
+using System.Linq;
+using System.Text;
+
+namespace Xacml
+{
+    public class CompositionDependencyResolver : IDependencyResolver
+    {
+        private CompositionContainer compositionContainer;
+
+        public CompositionDependencyResolver(CompositionContainer container)
+        {            
+            this.compositionContainer = container;
+        }
+
+        public void Register<T>(T value)
+        {
+            this.compositionContainer.ComposeExportedValue<T>(value);
+        }
+
+        public T GetService<T>()
+        {
+            return compositionContainer.GetExportedValue<T>();
+        }
+    }
+}
