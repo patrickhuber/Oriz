3dc751e4d340f16d1a0640a3c2e8f8fd4b29bd38
Patrick Huber <prhuber@gmail.com>
2014-01-26 21:40:20 -0500

    updates generation script

--- GenerateXacmlSchema.bat
+++ GenerateXacmlSchema.bat
@@ -1,3 +1,3 @@
 @Echo off
 REM xsd.exe must be in the path and local directory should be the directory of this batch file
-xsd SimpleXacml\xacml-core-v3-schema-wd-17.xsd /l:CS /n:SimpleXacml /c /o:SimpleXacml
+xsd Xacml\Schemas\xacml-core-v3-schema-wd-17.xsd /l:CS /n:Xacml.Schemas /c /o:Xacml\Schemas
