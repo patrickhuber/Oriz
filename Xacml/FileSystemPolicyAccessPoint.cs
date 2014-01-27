28fccd3a0555feab3c1d390d21f1340851d15800
Patrick Huber <prhuber@gmail.com>
2014-01-26 21:41:14 -0500

    updates classes to use schemas folder

--- Xacml/FileSystemPolicyAccessPoint.cs
+++ Xacml/FileSystemPolicyAccessPoint.cs
@@ -4,6 +4,7 @@ using System.IO;
 using System.Linq;
 using System.Text;
 using System.Xml.Serialization;
+using Xacml.Schemas;
 
 namespace Xacml
 {
--- Xacml/TargetExpression.cs
+++ Xacml/TargetExpression.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Xml;
+using Xacml.Schemas;
 
 namespace Xacml
 {
@@ -45,13 +46,13 @@ namespace Xacml
         private bool EvaluateMatch(MatchType match, IEnumerable<AttributesType> attributes)
         {
             if (match.Item is AttributeDesignatorType)
-                return EvaluateMatch(match, match.Item as AttributeDesignatorType, attributes);
+                return EvaluateDesignatorMatch(match, match.Item as AttributeDesignatorType, attributes);
             else if (match.Item is AttributeSelectorType)
-                return EvaluateMatch(match, match.Item as AttributeSelectorType, attributes);
+                return EvaluateSelectorMatch(match, match.Item as AttributeSelectorType, attributes);
             throw new InvalidOperationException("Unknown match item detected. Expected AttributeDesignator or AttributeSelector");
         }
 
-        private bool EvaluateMatch(MatchType matchType, AttributeDesignatorType designator, IEnumerable<AttributesType> attributes)
+        private bool EvaluateDesignatorMatch(MatchType matchType, AttributeDesignatorType designator, IEnumerable<AttributesType> attributes)
         {
             var matchingAttributes = FindMatchingAttributes(designator, attributes);
             throw new NotImplementedException();
@@ -59,24 +60,35 @@ namespace Xacml
 
         private IEnumerable<AttributeType> FindMatchingAttributes(AttributeDesignatorType designator, IEnumerable<AttributesType> attributes)
         {
-            foreach (var attributeList in attributes)
-            {
-                bool categoryMatch = attributeList.Category == designator.Category;
-                if (categoryMatch)
-                {
-                    foreach (var attribute in attributeList.Attribute)
-                    {
-                        bool attributeIdMatch = attribute.AttributeId == designator.AttributeId;
-                        if (attributeIdMatch)
-                        {
-                            yield return attribute;
-                        }
-                    }
-                }
-            }
+            return
+                from list in attributes
+                where list.Category == designator.Category
+                from a in list.Attribute
+                where a.AttributeId == designator.AttributeId
+                select a;
+
+            //var matches2 = attributes
+            //    .Where(list => list.Category == designator.Category)
+            //    .Select(list => list.Attribute.Where(a => a.AttributeId == designator.AttributeId));
+
+            //foreach (var attributeList in attributes)
+            //{
+            //    bool categoryMatch = attributeList.Category == designator.Category;
+            //    if (categoryMatch)
+            //    {
+            //        foreach (var attribute in attributeList.Attribute)
+            //        {
+            //            bool attributeIdMatch = attribute.AttributeId == designator.AttributeId;
+            //            if (attributeIdMatch)
+            //            {
+            //                yield return attribute;
+            //            }
+            //        }
+            //    }
+            //}
         }
 
-        private bool EvaluateMatch(MatchType matchType, AttributeSelectorType selectorType, IEnumerable<AttributesType> attributes)
+        private bool EvaluateSelectorMatch(MatchType matchType, AttributeSelectorType selectorType, IEnumerable<AttributesType> attributes)
         {
             throw new NotImplementedException();
         }
